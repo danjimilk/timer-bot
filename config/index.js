2ed4a664606f470431870f4a5533ee5132bad3d0
danjimilk <kangdanji@gmail.com>
2017-03-22 14:28:36 +0000

    Modified index.js and config

---
+++ config/index.js
@@ -0,0 +1,5 @@
+exports.SSL = {
+	"CA_PATH": "./../timer-bot_com.ssl/timer-bot_com.ca-bundle",
+	"KEY_PATH": "./../timer-bot_com.ssl/timer-bot_com.key",
+	"CERT_PATH": "./../timer-bot_com.ssl/timer-bot_com.crt"
+};
