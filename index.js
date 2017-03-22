ba7eded65e2313c957b187072aacf541a5dcc873
danjimilk <kangdanji@gmail.com>
2017-03-22 14:46:35 +0000

    Added request module

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const https = require('https');
 const express = require('express');
 const fs = require('fs');
-
 const config = require('./config');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.json());
+
+app.post('/hook', function (request, response) {
+	var eventObj = request.body.events[0];
+    var source = eventObj.source;
+    var message = eventObj.message;
+
+    // Request log
+    console.log('======================', new Date() ,'======================');
+    console.log('[request]', request.body);
+    console.log('[request source] ', eventObj.source);
+	console.log('[request message]', eventObj.message);
+	console.log('======================================================================');
+
+    response.sendStatus(200);
+});
 
 // set SSL
 var https_options = {
